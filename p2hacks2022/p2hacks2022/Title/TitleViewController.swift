2b4f09d041bac93971aac2e2582042e6602167e7
Takumi130902 <b1020239@fun.jp>
2022-12-14 23:08:18 +0900

    カスタムフォント追加

--- p2hacks2022/p2hacks2022/Title/TitleViewController.swift
+++ p2hacks2022/p2hacks2022/Title/TitleViewController.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class TitleViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-    
 
+    
     /*
     // MARK: - Navigation
 
@@ -25,5 +25,4 @@ class TitleViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
